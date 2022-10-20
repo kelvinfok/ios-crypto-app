c8bde462dd02e258bdf941d4ca152c42a0d076b3
kelvinfok <punkd1@gmail.com>
2022-10-20 21:55:18 +0800

    Initial Commit

---
+++ swiftcoin/swiftcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcoinApp.swift
+//  swiftcoin
+//
+//  Created by Kelvin Fok on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
